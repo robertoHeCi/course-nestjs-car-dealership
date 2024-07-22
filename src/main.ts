a4fad2a8cefe5748f176c8ed9e213e5e896f7307
roberto <roberto.hernandez@one-beyond.com>
2024-07-22 10:04:41 +0200

    feat: add validation pipe at app level

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // clean the data that is not in the DTO
+      forbidNonWhitelisted: true, // throw an error if the data is not in the DTO
+    }),
+  );
   await app.listen(3003);
 }
 bootstrap();
